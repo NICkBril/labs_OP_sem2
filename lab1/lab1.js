0842bf4aa735d747c9a061a74204190fd25d7727
Nick Bril <NickBril.ua@gmail.com>
2026-02-27 16:04:21 +0200

    fix lab1: remove comment slashes, make code runnable

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -1,47 +1,47 @@
-// // ______________________________________
-// // Task 1
+// ______________________________________
+// Task 1
 
-// function* generate(start = 1) {
-//     let i = start; 
+function* generate(start = 1) {
+    let i = start; 
 
-//     while (true) {
-//         yield i;
-//         i++;
-//   }
-// }
+    while (true) {
+        yield i;
+        i++;
+  }
+}
 
-// const gen = generate(5);
+const gen = generate(5);
 
-// console.log(gen.next().value);
-// console.log(gen.next().value);
-// console.log(gen.next().value);
-
-
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
 
 
 
-// // ______________________________________
-// // Task 2
 
-// function* generate(start = 1) {
-//     let i = start;
 
-//     while (true) {
-//         yield i++;
-//     }
-// }
+// ______________________________________
+// Task 2
 
-// function consumeWithTimeout(genObj, durationSeconds, pauseMs = 300) {
-//     const interval = setInterval(() => {
-//         const value = genObj.next().value;
-//         console.log(value);
-//     }, pauseMs);
+function* generate(start = 1) {
+    let i = start;
 
-//     setTimeout(() => {
-//         clearInterval(interval);
-//         console.log("The end");
-//     }, durationSeconds * 1000);
-// }
+    while (true) {
+        yield i++;
+    }
+}
 
-// const myGen = generate(5);
-// consumeWithTimeout(myGen, 3);
+function consumeWithTimeout(genObj, durationSeconds, pauseMs = 300) {
+    const interval = setInterval(() => {
+        const value = genObj.next().value;
+        console.log(value);
+    }, pauseMs);
+
+    setTimeout(() => {
+        clearInterval(interval);
+        console.log("The end");
+    }, durationSeconds * 1000);
+}
+
+const myGen = generate(5);
+consumeWithTimeout(myGen, 3);
