b2802f6a2a6cc51cc8b0e48c6399b71940668ea5
Nick Bril <NickBril.ua@gmail.com>
2026-04-03 16:03:04 +0300

    Implement dequeue and improve code structure

--- lab4/priorityQueue.js
+++ lab4/priorityQueue.js
@@ -8,21 +8,22 @@ class PriorityQueue {
   }
 
   peek(type) {
-    if (this.items.length === 0) {
-      return null;
+    const len = this.items.length;
+    if (len === 0) {
+        return null;
     }
 
     if (type === "oldest") {
-      return this.items[0];
+        return this.items[0];
     }
 
     if (type === "newest") {
-      return this.items[this.items.length - 1];
+        return this.items[len - 1];
     }
 
     if (type === "highest") {
       let high = this.items[0];
-      for (let i = 1; i < this.items.length; i++) {
+      for (let i = 1; i < len; i++) {
         if (this.items[i].priority > high.priority) {
           high = this.items[i];
         }
@@ -32,15 +33,42 @@ class PriorityQueue {
 
     if (type === "lowest") {
       let low = this.items[0];
-      for (let i = 1; i < this.items.length; i++) {
-        if (this.items[i].priority < low.priority) {
-          low = this.items[i];
+      for (let el of this.items) {
+        if (el.priority < low.priority) {
+            low = el;
         }
       }
       return low;
     }
 
     return null;
+  }
+
+  dequeue(type) {
+    if (this.items.length === 0) {
+        return null;
+    }    
+
+    switch (type) {
+      case "oldest": 
+        return this.items.shift();
+      case "newest": 
+        return this.items.pop();
+      case "highest":
+      case "lowest":
+        let targetIdx = 0;
+        for (let i = 1; i < this.items.length; i++) {
+          const current = this.items[i].priority;
+          const best = this.items[targetIdx].priority;
+          
+          if (type === "highest" ? current > best : current < best) {
+            targetIdx = i;
+          }
+        }
+        return this.items.splice(targetIdx, 1)[0];
+      default:
+        return null;
+    }
   }
 }
 
