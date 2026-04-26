f0a156a7cc18123b24d0225e875f406bdf1dd689
Nick Bril <NickBril.ua@gmail.com>
2026-04-26 19:33:30 +0300

    Add readline interface for line-by-line reading

--- lab6/stream-processing.js
+++ lab6/stream-processing.js
@@ -1,7 +1,17 @@
 const fs = require("fs");
+const readline = require("readline");
 
 function processFile(filePath) {
   const stream = fs.createReadStream(filePath);
+
+  const rl = readline.createInterface({
+    input: stream,
+    crlfDelay: Infinity,
+  });
+
+  rl.on("line", (line) => {
+    console.log(line);
+  });
 }
 
 module.exports = { processFile };
