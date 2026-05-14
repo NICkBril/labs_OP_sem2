da0bd1a07494f3226e69ffdc6241d08804a52399
Nick Bril <NickBril.ua@gmail.com>
2026-05-14 23:03:58 +0300

    Add async decorator support

--- lab9/log-decorator.js
+++ lab9/log-decorator.js
@@ -1,12 +1,52 @@
+const { performance } = require('perf_hooks');
+
 function createLogDecorator(logger) {
     return function log(options = {}) {
         const level = options.level || "INFO";
 
         return function (fn) {
             return function (...args) {
-                const res = fn.apply(this, args);
-                logger.log(level, `Method '${fn.name}' executed`, { args, result: res });
-                return res;
+                const start = performance.now();
+
+                const onSuccess = (result) => {
+                    const time = (performance.now() - start).toFixed(3);
+                    if (level !== "ERROR") {
+                        logger.log(level, `Method '${fn.name}' completed`, {
+                            duration: `${time}ms`,
+                            args,
+                            result
+                        });
+                    }
+                };
+
+                const onError = (err) => {
+                    logger.log("ERROR", `Method '${fn.name}' failed`, {
+                        args,
+                        error: err.message || err
+                    });
+                };
+
+                try {
+                    const res = fn.apply(this, args);
+
+                    if (res instanceof Promise) {
+                        return res
+                            .then(val => {
+                                onSuccess(val);
+                                return val;
+                            })
+                            .catch(err => {
+                                onError(err);
+                                throw err;
+                            });
+                    }
+
+                    onSuccess(res);
+                    return res;
+                } catch (err) {
+                    onError(err);
+                    throw err;
+                }
             };
         };
     };
