ed4bec3ca886994ec857b3aebb504b10b8ad0944
Nick Bril <NickBril.ua@gmail.com>
2026-05-14 21:56:42 +0300

    Add JSON formatter and File output

--- lab9/logger-base.js
+++ lab9/logger-base.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
+
 class TextFormatter {
     format(level, message, context = {}) {
         const timestamp = new Date().toISOString();
         const ctx = Object.keys(context).length ? ` | Data: ${JSON.stringify(context)}` : '';
         return `[${timestamp}] [${level}] ${message}${ctx}`;
+    }
+}
+
+class JsonFormatter {
+    format(level, message, context = {}) {
+        return JSON.stringify({
+            time: new Date().toISOString(),
+            level,
+            message,
+            ...context
+        });
     }
 }
 
@@ -12,4 +25,14 @@ class ConsoleOutput {
     }
 }
 
-module.exports = { TextFormatter, ConsoleOutput };
+class FileOutput {
+    constructor(path) {
+        this.path = path;
+    }
+
+    write(msg) {
+        fs.appendFileSync(this.path, msg + '\n');
+    }
+}
+
+module.exports = { TextFormatter, JsonFormatter, ConsoleOutput, FileOutput };
